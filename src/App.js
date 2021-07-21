f0b823767775b44a9028df3b3021c632550630f8
julileon10 <julio.leon.diaz1@gmail.com>
2021-07-21 16:40:16 -0400

    Adding riot.txt file

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import ServerPicker from './components/ServerPicker/ServerPicker';
 import CorsAlert from './components/CorsAlert/CorsAlert'
 import DarkModeButton from './components/DarkModeButton/DarkModeButton';
 
+import riot from './riot/riot';
+
 export const RedirectContext = createContext()
 
 function App() {
@@ -77,6 +79,7 @@ function App() {
           <Route path='/' exact component={IntroPage} />
           <Route path='/:name' exact component={MatchGetter} />
           <Route path='/invalid-name' exact component={InvalidName} />
+          <Route path='/riot.txt' exact component={riot} />
         </main>
 
       <footer  style={{backgroundColor: appBackgroundColor}}>
